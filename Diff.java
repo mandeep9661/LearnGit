11c083c8edba63ae56ea6e03ccdc6c620f1790a1
Mandeep Kumar <mkc9661@gmail.com>
2022-01-09 16:59:12 +0530

    Adding Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,9 @@
 public class Diff{
-    
+
+    public static void main(String[] args){
+        int a = 5;
+        int b = 7;
+
+        System.out.println("The difference is " + (a-b));
+    }
 }
